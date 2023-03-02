2c68d90ec6b0ecdd83639422ce4bb18bfcada747
corno <corno.astn.a@schraverus.com>
2023-03-02 12:46:32 +0100

    .

--- pareto/src/data/project.data.ts
+++ pareto/src/data/project.data.ts
@@ -9,7 +9,7 @@ import { $ as glossary } from "./glossary.data"
 
 export const $: gproject.T.Project<pd.SourceLocation> = {
     'author': "Corno",
-    'description': "a pareto wrapper around basic string operations as Pareto lacks them (yes, really, by design, the idea is to find a way how not to need them, and if you really do, then use this library)",
+    'description': "a pareto wrapper around basic dictionary operations as Pareto lacks them (yes, really, by design, the idea is to find a way how not to need them, and if you really do, then use this library)",
     'license': "TBD",
     
     'dependencies': d({
