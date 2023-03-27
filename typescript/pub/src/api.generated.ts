60c85737b32417129c1c4dc303693fff1ba5acbc
corno <corno.astn.a@schraverus.com>
2023-03-27 13:44:35 +0200

    .

--- typescript/pub/src/api.generated.ts
+++ typescript/pub/src/api.generated.ts
@@ -2,6 +2,10 @@ import * as pt from 'pareto-core-types'
 
 import * as g_this from "./glossary"
 
+export namespace D {
+    
+}
+
 export namespace A {
     
     export type isEmpty = () => g_this.SYNC.A.F.IsEmpty
